d621f19026f0c2a252d05bcf02f23a375af22b23
IamArbabMemon <139270412+IamArbabMemon@users.noreply.github.com>
2025-02-03 02:26:13 +0500

    'commit'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get("/greet",(req,res)=>{
-    return res.json({message:"Good night Friends"});
+    return res.json({message:"Good Bye Friends I am very happy"});
 })
 
 app.listen(3000,()=>{
