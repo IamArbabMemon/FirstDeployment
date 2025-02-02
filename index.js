17d8c57e615a356fe8b32e24a334978ab5db012d
IamArbabMemon <139270412+IamArbabMemon@users.noreply.github.com>
2025-02-03 02:18:23 +0500

    new

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get("/greet",(req,res)=>{
-    return res.json({message:"Good morning"});
+    return res.json({message:"Good night"});
 })
 
 app.listen(3000,()=>{
